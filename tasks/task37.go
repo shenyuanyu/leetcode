1cec5499753cf0d5f7e169cb3c5e38cc66c15f58
shenyuanyu <shenyuanyu@ghostcloud.cn>
2018-12-05 21:29:09 +0800

    实现基本逻辑

--- tasks/task37.go
+++ tasks/task37.go
@@ -1,6 +1,7 @@
 package tasks
 
 import (
+	"fmt"
 	"reflect"
 )
 
@@ -10,10 +11,50 @@ type position struct {
 }
 
 // 解决数独问题
-func solveSudoku(board [][]byte) {
+func SolveSudoku(board [][]byte) {
+	notSolvePosPossibleMap := make(map[position]map[byte]struct{})
+	// 初始化
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			k := board[i][j]
+			if k == '.' { // 未确定
+				pos := position{i, j}
+				notSolvePosPossibleMap[pos] = generateMapByByteRange('1', '9')
 
+				excludeByRow(pos, notSolvePosPossibleMap, board)
+				excludeByCol(pos, notSolvePosPossibleMap, board)
+				excludeByTinySudoku(pos, notSolvePosPossibleMap, board)
+
+				if len(notSolvePosPossibleMap[pos]) == 1 {
+					for key := range notSolvePosPossibleMap[pos] {
+						board[i][j] = key
+						delete(notSolvePosPossibleMap, pos)
+						break
+					}
+				}
+			}
+		}
+	}
+
+	// 循环检测
+	for len(notSolvePosPossibleMap) != 0 {
+		for pos := range notSolvePosPossibleMap {
+			excludeByRow(pos, notSolvePosPossibleMap, board)
+			excludeByCol(pos, notSolvePosPossibleMap, board)
+			excludeByTinySudoku(pos, notSolvePosPossibleMap, board)
+
+			if len(notSolvePosPossibleMap[pos]) == 1 {
+				for key := range notSolvePosPossibleMap[pos] {
+					board[pos.Row][pos.Col] = key
+					delete(notSolvePosPossibleMap, pos)
+					break
+				}
+			}
+		}
+	}
 }
 
+// 生成start到end的byte数组, [start, end]
 func generateMapByByteRange(start, end byte) map[byte]struct{} {
 	hash := make(map[byte]struct{})
 	for i := start; i <= end; i++ {
@@ -33,22 +74,80 @@ func excludeByRow(pos position, notSolvePosPossible map[position]map[byte]struct
 		}
 
 		k := board[pos.Row][j]
-		if k == '.' {
+		if k == '.' { // 未确定位置, 加入队列
 			notSolvePos = append(notSolvePos, position{pos.Row, j})
-		} else {
+		} else { // 已确定位置, 直接排除
 			if _, ok := posPossible[k]; ok {
 				delete(posPossible, k)
 			}
 		}
 	}
 
+	excludeByPossible(posPossible, notSolvePos, notSolvePosPossible)
 }
 
-func excludeByPossible(notSolvePos []position, notSolvePosPossibleMap map[position]map[byte]struct{}) map[byte]struct{} {
+// 根据列排除可能性
+func excludeByCol(pos position, notSolvePosPossible map[position]map[byte]struct{}, board [][]byte) {
+	posPossible := notSolvePosPossible[pos]
+
+	notSolvePos := make([]position, 0, 9)
+	for i := 0; i < 9; i++ {
+		if pos.Row == i {
+			continue
+		}
+
+		k := board[i][pos.Col]
+		if k == '.' { // 未确定位置, 加入队列
+			notSolvePos = append(notSolvePos, position{i, pos.Col})
+		} else { // 已确定位置, 直接排除
+			if _, ok := posPossible[k]; ok {
+				delete(posPossible, k)
+			}
+		}
+	}
+
+	excludeByPossible(posPossible, notSolvePos, notSolvePosPossible)
+}
+
+// 根据小九宫排除
+func excludeByTinySudoku(pos position, notSolvePosPossible map[position]map[byte]struct{}, board [][]byte) {
+	posPossible := notSolvePosPossible[pos]
+
+	// 确定小九宫起始位置
+	row := pos.Row / 3 * 3
+	col := pos.Col / 3 * 3
+
+	notSolvePos := make([]position, 0, 9)
+	for i := row; i < row+3; i++ {
+		for j := col; j < col+3; j++ {
+			if i == pos.Row && j == pos.Col {
+				continue
+			}
+
+			k := board[i][j]
+			if k == '.' { // 未确定
+				notSolvePos = append(notSolvePos, position{i, j})
+			} else {
+				if _, ok := posPossible[k]; ok {
+					delete(posPossible, k)
+				}
+			}
+		}
+	}
+
+	// 可能性排除法
+	excludeByPossible(posPossible, notSolvePos, notSolvePosPossible)
+}
+
+func excludeByPossible(posPossibleMap map[byte]struct{},
+	notSolvePos []position,
+	notSolvePosPossibleMap map[position]map[byte]struct{}) {
+
+	// 计算每个可能出现率
 	count := make([]int, 0, len(notSolvePos))
 	for i := 0; i < len(notSolvePos)-1; i++ {
 		k := 0
-		for j := i; j < len(notSolvePos); j++ {
+		for j := i + 1; j < len(notSolvePos); j++ {
 			if reflect.DeepEqual(notSolvePosPossibleMap[notSolvePos[i]], notSolvePosPossibleMap[notSolvePos[j]]) {
 				k++
 				notSolvePos = deletePositionArray(notSolvePos, j)
@@ -58,10 +157,27 @@ func excludeByPossible(notSolvePos []position, notSolvePosPossibleMap map[positi
 		count = append(count, k)
 	}
 
-	i
-
+	// 根据出现率删除
+	for i := 0; i < len(count); i++ {
+		if len(notSolvePosPossibleMap[notSolvePos[i]]) == count[i] {
+			for key := range notSolvePosPossibleMap[notSolvePos[i]] {
+				delete(posPossibleMap, key)
+			}
+		}
+	}
 }
 
 func deletePositionArray(arr []position, i int) []position {
 	return append(arr[:i], arr[i+1:]...)
+}
+
+// PrintBoard ...
+func PrintBoard(board [][]byte) {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			fmt.Printf("%s\t", string(board[i][j]))
+		}
+		fmt.Println()
+
+	}
 }
