09e9b99b1fa479c2465aa4966825e8267fc03845
shenyuanyu <shenyuanyu@ghostcloud.cn>
2018-12-12 16:07:57 +0800

    完成第41号题目

---
+++ tasks/task41.go
@@ -0,0 +1,19 @@
+package tasks
+
+func firstMissingPositive(nums []int) int {
+	miss := 1
+	hash := make(map[int]struct{})
+	for _, num := range nums {
+		hash[num] = struct{}{}
+
+		for {
+			if _, ok := hash[miss]; ok {
+				miss++
+			} else {
+				break
+			}
+		}
+	}
+
+	return miss
+}
---
+++ tasks/task41_test.go
@@ -0,0 +1,24 @@
+package tasks
+
+import "testing"
+
+func Test_firstMissingPositive(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"c1", args{[]int{3, 4, -1, 1}}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstMissingPositive(tt.args.nums); got != tt.want {
+				t.Errorf("firstMissingPositive() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
