fe5f4048965cea49c1ff8e5060dddb784cb42f9c
shenyuanyu <shenyuanyu@ghostcloud.cn>
2018-12-24 21:03:38 +0800

    完成第46题

---
+++ tasks/task46.go
@@ -0,0 +1,23 @@
+package tasks
+
+func permute(nums []int) [][]int {
+	if len(nums) == 0 {
+		return [][]int{}
+	}
+	if len(nums) == 1 {
+		return [][]int{nums}
+	}
+
+	revPermutes := [][]int{}
+	prevPermutes := permute(nums[1:])
+	for _, prevPermute := range prevPermutes {
+		for i := 0; i <= len(prevPermute); i++ {
+			t := append([]int{}, prevPermute[:i]...)
+			t = append(t, nums[0])
+			t = append(t, prevPermute[i:]...)
+			revPermutes = append(revPermutes, t)
+		}
+	}
+
+	return revPermutes
+}
