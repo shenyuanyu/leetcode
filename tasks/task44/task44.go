5321854761bda2446d036942c3bfce2a427a8ebb
shenyuanyu <shenyuanyu@ghostcloud.cn>
2019-02-12 18:42:53 +0800

    完成部分44题

--- tasks/task44/task44.go
+++ tasks/task44/task44.go
@@ -3,30 +3,66 @@ package task44
 import "strings"
 
 func isMatch(s string, p string) bool {
-	if len(s) < len(p) {
-		return false
-	}
-
 	patterns := splitPattern(p)
 
-}
-
-func match(s string, patterns []string) bool {
 	if len(s) < stringSliceLen(patterns) {
 		return false
 	}
-
-	for _, pattern := range patterns {
-		if !isWildCardPattern(pattern) {
-			l := len(pattern)
-			if s[:l] == pattern {
-				return match(s[l:], patterns)
-			}
-		}
-
-	}
+	return match(s, patterns)
 }
 
+// match ...
+func match(s string, patterns []string) bool {
+	if len(s) < stringSliceLen(patterns) { // 是否够长
+		return false
+	}
+
+	if len(s) == 0 {
+		return true
+	}
+
+	if len(patterns) == 0 {
+		return false
+	}
+
+	if !isWildCardPattern(patterns[0]) { // 不是通配符
+		if len(s) == len(patterns[0]) { // 长度相等
+			if equal(s, patterns[0]) {
+				for i := 1; i < len(patterns); i++ {
+					if patterns[i] == "*" {
+						continue
+					}
+					return false
+				}
+				return true
+			}
+			return false
+		}
+
+		// 长度不等
+		if !equal(s[:len(patterns[0])], patterns[0]) {
+			return false
+		}
+		// 递归
+		return match(s[len(patterns[0]):], patterns[1:])
+	}
+
+	// 通配符
+	if stringSliceLen(patterns) == 0 {
+		return true
+	}
+	for i := 0; i < len(s)+1-len(patterns[1]); i++ {
+		if equal(s[i:i+len(patterns[1])], patterns[1]) {
+			if match(s[i+len(patterns[1]):], patterns[2:]) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// splitPattern 分离pattern
 func splitPattern(p string) []string {
 	var patterns []string
 	var ptnBuilder strings.Builder
@@ -36,7 +72,14 @@ func splitPattern(p string) []string {
 			continue
 		}
 
-		patterns = append(patterns, ptnBuilder.String(), "*")
+		if i == 0 {
+			patterns = append(patterns, "*")
+		}
+
+		if ptnBuilder.Len() != 0 {
+			patterns = append(patterns, ptnBuilder.String(), "*")
+		}
+
 		ptnBuilder.Reset()
 	}
 
@@ -46,27 +89,36 @@ func splitPattern(p string) []string {
 	return patterns
 }
 
+// equal 判断字符串与模式是否匹配
 func equal(s string, p string) bool {
 	if p == "*" {
 		return true
 	}
-	if s == p {
-		return true
+	if len(s) != len(p) {
+		return false
 	}
-	if p[len(p)-1] == '?' {
-		return s[:len(s)-1] == p[:len(p)-1]
+
+	for i := 0; i < len(s); i++ {
+		if s[i] != p[i] && p[i] != '?' {
+			return false
+		}
 	}
-	return false
+
+	return true
 }
 
+// isWildCardPattern 判断是否为模式
 func isWildCardPattern(p string) bool {
 	return p == "*"
 }
 
+// stringSliceLen 模式字符串切片中, 所有定长模式字符串之和
 func stringSliceLen(sSlice []string) int {
 	l := 0
 	for _, s := range sSlice {
-		l += len(s)
+		if !isWildCardPattern(s) {
+			l += len(s)
+		}
 	}
 	return l
 }
---
+++ tasks/task44/task44_test.go
@@ -0,0 +1,25 @@
+package task44
+
+import "testing"
+
+func Test_isMatch(t *testing.T) {
+	type args struct {
+		s string
+		p string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{"c1", args{"abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb", "**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isMatch(tt.args.s, tt.args.p); got != tt.want {
+				t.Errorf("isMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
