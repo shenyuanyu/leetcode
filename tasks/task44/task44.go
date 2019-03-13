e58e5554678dc0435745e1dfc0a7b160e0dad14f
shenyuanyu <shenyuanyu@ghostcloud.cn>
2019-03-13 18:17:17 +0800

    更新第44题

--- tasks/task44/task44.go
+++ tasks/task44/task44.go
@@ -3,122 +3,93 @@ package task44
 import "strings"
 
 func isMatch(s string, p string) bool {
+	p = pretreatment(p)
 	patterns := splitPattern(p)
 
-	if len(s) < stringSliceLen(patterns) {
-		return false
-	}
-	return match(s, patterns)
+	return subIsMatch(s, patterns)
 }
 
-// match ...
-func match(s string, patterns []string) bool {
-	if len(s) < stringSliceLen(patterns) { // 是否够长
-		return false
-	}
-
-	if len(s) == 0 {
-		return true
-	}
-
+func subIsMatch(s string, patterns []string) bool {
 	if len(patterns) == 0 {
-		return false
+		return len(s) == 0
 	}
 
-	if !isWildCardPattern(patterns[0]) { // 不是通配符
-		if len(s) == len(patterns[0]) { // 长度相等
-			if equal(s, patterns[0]) {
-				for i := 1; i < len(patterns); i++ {
-					if patterns[i] == "*" {
-						continue
-					}
-					return false
-				}
-				return true
-			}
+	if patterns[0] != "*" {
+		if len(s) < len(patterns[0]) || !equalPattern(s[:len(patterns[0])], patterns[0]) {
 			return false
 		}
-
-		// 长度不等
-		if !equal(s[:len(patterns[0])], patterns[0]) {
-			return false
-		}
-		// 递归
-		return match(s[len(patterns[0]):], patterns[1:])
+		return subIsMatch(s[len(patterns[0]):], patterns[1:])
 	}
 
-	// 通配符
-	if stringSliceLen(patterns) == 0 {
+	if len(patterns) == 1 {
 		return true
 	}
-	for i := 0; i < len(s)+1-len(patterns[1]); i++ {
-		if equal(s[i:i+len(patterns[1])], patterns[1]) {
-			if match(s[i+len(patterns[1]):], patterns[2:]) {
-				return true
-			}
+
+	for i := 0; i < len(s); i++ {
+		if subIsMatch(s[i:], patterns[1:]) {
+			return true
 		}
 	}
 
 	return false
 }
 
-// splitPattern 分离pattern
-func splitPattern(p string) []string {
-	var patterns []string
-	var ptnBuilder strings.Builder
-	for i := 0; i < len(p); i++ {
+func pretreatment(p string) string {
+	var str strings.Builder
+	for i := 0; i < len(p); {
+		str.WriteByte(p[i])
 		if p[i] != '*' {
-			ptnBuilder.WriteByte(p[i])
+			i++
 			continue
 		}
 
-		if i == 0 {
-			patterns = append(patterns, "*")
+		i++
+		for i < len(p) && p[i] == '*' {
+			i++
 		}
-
-		if ptnBuilder.Len() != 0 {
-			patterns = append(patterns, ptnBuilder.String(), "*")
-		}
-
-		ptnBuilder.Reset()
 	}
 
-	if ptnBuilder.Len() != 0 {
-		patterns = append(patterns, ptnBuilder.String())
+	return str.String()
+}
+
+func splitPattern(p string) []string {
+	patterns := make([]string, 0, 10)
+	var str strings.Builder
+	for _, c := range p {
+		if c != '*' {
+			str.WriteRune(c)
+			continue
+		}
+
+		if str.Len() != 0 {
+			patterns = append(patterns, str.String(), string(c))
+		} else {
+			patterns = append(patterns, string(c))
+		}
+
+		str.Reset()
 	}
+
+	if str.Len() != 0 {
+		patterns = append(patterns, str.String())
+	}
+
 	return patterns
 }
 
-// equal 判断字符串与模式是否匹配
-func equal(s string, p string) bool {
-	if p == "*" {
-		return true
-	}
-	if len(s) != len(p) {
-		return false
-	}
-
-	for i := 0; i < len(s); i++ {
-		if s[i] != p[i] && p[i] != '?' {
+func equalPattern(s string, p string) bool {
+	if p != "*" {
+		if len(s) != len(p) {
 			return false
 		}
+
+		for i := 0; i < len(s); i++ {
+			if s[i] != p[i] && p[i] != '?' {
+				return false
+			}
+		}
+		return true
 	}
 
 	return true
-}
-
-// isWildCardPattern 判断是否为模式
-func isWildCardPattern(p string) bool {
-	return p == "*"
-}
-
-// stringSliceLen 模式字符串切片中, 所有定长模式字符串之和
-func stringSliceLen(sSlice []string) int {
-	l := 0
-	for _, s := range sSlice {
-		if !isWildCardPattern(s) {
-			l += len(s)
-		}
-	}
-	return l
 }
