50552465893166a0762283a13fc648d05dea55ca
shenyuanyu <shenyuanyu@ghostcloud.cn>
2019-06-26 18:23:39 +0800

    task: 完成第50题

--- tasks/task50/task50.go
+++ tasks/task50/task50.go
@@ -1,6 +1,25 @@
 package task50
 
 func myPow(x float64, n int) float64 {
+	if x == 0 {
+		return 0
+	}
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return x
+	}
+	if n < 0 {
+		n = -n
+		x = 1 / x
+	}
 
-	return 0
+	t := myPow(x, n/2)
+	t *= t
+	if n%2 != 0 {
+		t *= x
+	}
+
+	return t
 }
---
+++ tasks/task50/task50_test.go
@@ -0,0 +1,26 @@
+package task50
+
+import "testing"
+
+func Test_myPow(t *testing.T) {
+	type args struct {
+		x float64
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		// TODO: Add test cases.
+		{"c1", args{2, 4}, 16},
+		{"c1", args{34.00515, -3}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := myPow(tt.args.x, tt.args.n); got != tt.want {
+				t.Errorf("myPow() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
