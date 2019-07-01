cf213045cbbdfdacf4dc3e2da9343a9a83cadf69
shenyuanyu <shenyuanyu@ghostcloud.cn>
2019-07-01 11:04:14 +0800

    task37部分完成

--- tasks/task37/task37.go
+++ tasks/task37/task37.go
@@ -2,260 +2,74 @@ package task37
 
 import (
 	"fmt"
-	"reflect"
 )
 
-type position struct {
-	Row int
-	Col int
+func solveSudoku(board [][]byte) {
+	var rowUsed [9]uint16
+	var colUsed [9]uint16
+	var minSudokuUsed [9]uint16
+
+	isSolved := false
+	for !isSolved {
+		isSolved = recursiveSolveSudoku(&rowUsed, &colUsed, &minSudokuUsed, board)
+	}
 }
 
-// 解决数独问题
-func SolveSudoku(board [][]byte) {
-	notSolvePosPossibleMap := make(map[position]map[byte]struct{})
-	// 初始化
+func recursiveSolveSudoku(rowUsed *[9]uint16, colUsed *[9]uint16, minSudokuUsed *[9]uint16, board [][]byte) bool {
+	isSolved := false
+
 	for i := 0; i < 9; i++ {
 		for j := 0; j < 9; j++ {
-			k := board[i][j]
-			if k == '.' { // 未确定
-				pos := position{i, j}
-				notSolvePosPossibleMap[pos] = generateMapByByteRange('1', '9')
-
-				excludeByRow(pos, notSolvePosPossibleMap, board)
-				excludeByCol(pos, notSolvePosPossibleMap, board)
-				excludeByTinySudoku(pos, notSolvePosPossibleMap, board)
-
-				if len(notSolvePosPossibleMap[pos]) == 1 {
-					for key := range notSolvePosPossibleMap[pos] {
-						board[i][j] = key
-						delete(notSolvePosPossibleMap, pos)
-						break
-					}
+			if board[i][j] == '.' {
+				t := ^rowUsed[i] & ^colUsed[j] & ^minSudokuUsed[getMinSudokuIndex(i, j)]
+				rs := toBytes(t)
+				if len(rs) == 1 {
+					board[i][j] = rs[0]
 				}
 			}
-		}
-	}
 
-	// 循环检测
-	prev := 0
-	for len(notSolvePosPossibleMap) != 0 && prev != len(notSolvePosPossibleMap) {
-		prev = len(notSolvePosPossibleMap)
-		for pos := range notSolvePosPossibleMap {
-			excludeByRow(pos, notSolvePosPossibleMap, board)
-			excludeByCol(pos, notSolvePosPossibleMap, board)
-			excludeByTinySudoku(pos, notSolvePosPossibleMap, board)
-
-			if len(notSolvePosPossibleMap[pos]) == 1 {
-				for key := range notSolvePosPossibleMap[pos] {
-					board[pos.Row][pos.Col] = key
-					delete(notSolvePosPossibleMap, pos)
-					break
-				}
-			}
-		}
-	}
-
-	PrintPossibleMap(notSolvePosPossibleMap)
-}
-
-// 生成start到end的byte数组, [start, end]
-func generateMapByByteRange(start, end byte) map[byte]struct{} {
-	hash := make(map[byte]struct{})
-	for i := start; i <= end; i++ {
-		hash[i] = struct{}{}
-	}
-	return hash
-}
-
-// 根据行排除可能性
-func excludeByRow(pos position, notSolvePosPossibleMap map[position]map[byte]struct{}, board [][]byte) {
-	posPossible := notSolvePosPossibleMap[pos]
-
-	notSolvePos := make([]position, 0, 9)
-	for j := 0; j < 9; j++ {
-		if pos.Col == j {
-			continue
-		}
-
-		k := board[pos.Row][j]
-		if k == '.' { // 未确定位置, 加入队列
-			notSolvePos = append(notSolvePos, position{pos.Row, j})
-		} else { // 已确定位置, 直接排除
-			if _, ok := posPossible[k]; ok {
-				delete(posPossible, k)
-			}
-		}
-	}
-
-	excludeByPossible(pos, notSolvePos, notSolvePosPossibleMap)
-	excludeByNotPossible(pos, notSolvePos, notSolvePosPossibleMap)
-}
-
-// 根据列排除可能性
-func excludeByCol(pos position, notSolvePosPossible map[position]map[byte]struct{}, board [][]byte) {
-
-	posPossible := notSolvePosPossible[pos]
-
-	notSolvePos := make([]position, 0, 9)
-	for i := 0; i < 9; i++ {
-		if pos.Row == i {
-			continue
-		}
-
-		k := board[i][pos.Col]
-		if k == '.' { // 未确定位置, 加入队列
-			notSolvePos = append(notSolvePos, position{i, pos.Col})
-		} else { // 已确定位置, 直接排除
-			if _, ok := posPossible[k]; ok {
-				delete(posPossible, k)
-			}
-		}
-	}
-
-	excludeByPossible(pos, notSolvePos, notSolvePosPossible)
-	excludeByNotPossible(pos, notSolvePos, notSolvePosPossible)
-
-}
-
-// 根据小九宫排除
-func excludeByTinySudoku(pos position, notSolvePosPossible map[position]map[byte]struct{}, board [][]byte) {
-	posPossible := notSolvePosPossible[pos]
-
-	// 确定小九宫起始位置
-	row := pos.Row / 3 * 3
-	col := pos.Col / 3 * 3
-
-	notSolvePos := make([]position, 0, 9)
-	for i := row; i < row+3; i++ {
-		for j := col; j < col+3; j++ {
-			if i == pos.Row && j == pos.Col {
-				continue
-			}
-
-			k := board[i][j]
-			if k == '.' { // 未确定
-				notSolvePos = append(notSolvePos, position{i, j})
+			if board[i][j] != '.' {
+				var t uint16 = 1 << (board[i][j] - '1')
+				rowUsed[i] |= t
+				colUsed[j] |= t
+				minSudokuUsed[getMinSudokuIndex(i, j)] |= t
 			} else {
-				if _, ok := posPossible[k]; ok {
-					delete(posPossible, k)
-				}
+				isSolved = false
 			}
 		}
 	}
 
-	// 可能性排除法
-	excludeByPossible(pos, notSolvePos, notSolvePosPossible)
-	excludeByNotPossible(pos, notSolvePos, notSolvePosPossible)
+	printSudoku(board)
+	return isSolved
 }
 
-func excludeByPossible(pos position, notSolvePos []position,
-	notSolvePosPossibleMap map[position]map[byte]struct{}) {
-
-	posPossibleMap := notSolvePosPossibleMap[pos]
-
-	if len(posPossibleMap) == 0 {
-		panic("this is zero error")
-	}
-
-	if len(posPossibleMap) == 1 {
-		return
-	}
-
-	// 计算每个可能的出现率
-	count := make([]int, 0, len(notSolvePos))
-	for i := 0; i < len(notSolvePos)-1; i++ {
-		k := 1
-		for j := i + 1; j < len(notSolvePos); j++ {
-			if reflect.DeepEqual(notSolvePosPossibleMap[notSolvePos[i]], notSolvePosPossibleMap[notSolvePos[j]]) {
-				k++
-				notSolvePos = deletePositionArray(notSolvePos, j)
-				j--
-			}
-		}
-		count = append(count, k)
-	}
-
-	// 根据出现率删除
-	for i := 0; i < len(count); i++ {
-		if len(notSolvePosPossibleMap[notSolvePos[i]]) == count[i] {
-			for key := range notSolvePosPossibleMap[notSolvePos[i]] {
-				delete(posPossibleMap, key)
-			}
-		}
-		if len(notSolvePosPossibleMap[notSolvePos[i]]) != 0 && len(notSolvePosPossibleMap[notSolvePos[i]]) < count[i] {
-			fmt.Println(notSolvePosPossibleMap[notSolvePos[i]])
-			fmt.Println(count[i])
-			panic("have possible error")
-		}
-	}
-
+func getMinSudokuIndex(i, j int) int {
+	return i/3*3 + j/3
 }
 
-func excludeByNotPossible(pos position, notSolvePos []position,
-	notSolvePosPossibleMap map[position]map[byte]struct{}) {
+func toBytes(candidate uint16) []byte {
+	var j uint16 = 1
 
-	posPossibleMap := notSolvePosPossibleMap[pos]
-
-	if len(posPossibleMap) == 0 {
-		panic("this is zero error")
-	}
-	if len(posPossibleMap) == 1 {
-		return
-	}
-
-	for posPossible := range posPossibleMap {
-		b := true
-		for _, notPos := range notSolvePos {
-			notSolvePosPossible, ok := notSolvePosPossibleMap[notPos]
-			if ok {
-				if _, ok := notSolvePosPossible[posPossible]; ok {
-					b = false
-				}
-			} else {
-				return
-			}
-
+	var resp []byte
+	var i byte = '1'
+	for ; i <= '9'; i++ {
+		if candidate&j != 0 {
+			resp = append(resp, i)
 		}
 
-		if b {
-			notSolvePosPossibleMap[pos] = map[byte]struct{}{
-				posPossible: {},
-			}
-			return
-		}
+		j <<= 1
 	}
 
+	return resp
 }
 
-func deletePositionArray(arr []position, i int) []position {
-	rev := make([]position, 0, len(arr))
-
-	for k := 0; k < len(arr); k ++ {
-		if k != i {
-			rev = append(rev, arr[k])
-		}
-	}
-
-	return rev
-}
-
-// PrintBoard ...
-func PrintBoard(board [][]byte) {
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
-			fmt.Printf("%s\t", string(board[i][j]))
-		}
-		fmt.Println()
-
-	}
-}
-
-func PrintPossibleMap(possibleMap map[position]map[byte]struct{}) {
-	for pos := range possibleMap {
-		fmt.Printf("\tPosition: [%v]\n\t\t", pos)
-		for key := range possibleMap[pos] {
-			fmt.Printf("%s, ", string(key))
+func printSudoku(board [][]byte) {
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board); j++ {
+			fmt.Printf("%c\t", board[i][j])
 		}
 		fmt.Println()
 	}
+
+	fmt.Println("------------------------------------")
 }
--- tasks/task37/task37_test.go
+++ tasks/task37/task37_test.go
@@ -1,100 +1,43 @@
 package task37
 
-import (
-	"reflect"
-	"testing"
-)
+import "testing"
 
-func Test_generateMapByByteRange(t *testing.T) {
+func Test_solveSudoku(t *testing.T) {
 	type args struct {
-		start byte
-		end   byte
+		board [][]byte
 	}
 	tests := []struct {
 		name string
 		args args
-		want map[byte]struct{}
 	}{
 		// TODO: Add test cases.
-		{
-			"c1",
-			args{'1', '9'},
-			map[byte]struct{}{
-				'1': struct{}{},
-				'2': struct{}{},
-				'3': struct{}{},
-				'4': struct{}{},
-				'5': struct{}{},
-				'6': struct{}{},
-				'7': struct{}{},
-				'8': struct{}{},
-				'9': struct{}{},
-			},
-		},
+		{"c1", args{board: [][]byte{
+			{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+			{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+			{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+			{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+			{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+			{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+			{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+			{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+			{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+		}}},
+		{"c2", args{board: [][]byte{
+			{'.', '.', '9', '7', '4', '8', '.', '.', '.'},
+			{'7', '.', '.', '.', '.', '.', '.', '.', '.'},
+			{'.', '2', '.', '1', '.', '9', '.', '.', '.'},
+			{'.', '.', '7', '.', '.', '.', '2', '4', '.'},
+			{'.', '6', '4', '.', '1', '.', '5', '9', '.'},
+			{'.', '9', '8', '.', '.', '.', '3', '.', '.'},
+			{'.', '.', '.', '8', '.', '3', '.', '2', '.'},
+			{'.', '.', '.', '.', '.', '.', '.', '.', '6'},
+			{'.', '.', '.', '2', '7', '5', '9', '.', '.'},
+		}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := generateMapByByteRange(tt.args.start, tt.args.end); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("generateMapByByteRange() = %v, want %v", got, tt.want)
-			}
-			t.Logf("%s case success, generate [%v]", tt.name, tt.want)
+			solveSudoku(tt.args.board)
+			t.Log(tt.args.board)
 		})
 	}
-}
-
-func Test_deletePositionArray(t *testing.T) {
-	// case1
-	arr := []position{{0, 1}, {0, 2}, {0, 3}, {0, 4}}
-	i := 2
-
-	out := deletePositionArray(arr, i)
-
-	t.Logf("the result is: [%v], the origin is: [%v]", out, arr)
-}
-
-func Test_excludeByPossible(t *testing.T) {
-	pos := position{0, 1}
-	notSolvePosArry := []position{{0, 2}, {0, 4}, {0, 5},{0, 7}}
-	notSolvePosPossibleMap := map[position]map[byte]struct{}{
-		{0, 1}: {
-			'2': struct{}{},
-			'3': struct{}{},
-			'4': struct{}{},
-			'5': struct{}{},
-		},
-		{0, 2}: {
-			'2': struct{}{},
-			'3': struct{}{},
-		},
-		{0, 4}: {
-			'2': struct{}{},
-			'3': struct{}{},
-		},
-	}
-
-	excludeByPossible(pos, notSolvePosArry, notSolvePosPossibleMap)
-
-	t.Logf("the possible is: [%v]", notSolvePosPossibleMap[pos])
-}
-
-func Test_excludeByNotPossible(t *testing.T) {
-	pos := position{0, 1}
-	notSolvePos := []position{{0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6},
-		{0, 7}, {0, 8}, {0, 9}}
-	notSolvePosPossibleMap := map[position]map[byte]struct{}{
-		{0,1}: {'1': {}, '2': {}, '3': {}},
-		{0,2}: {'1': {}, '3': {}},
-		{0,3}: {'4': {}, '3': {}},
-		{0,4}: {'5': {}, '3': {}},
-		{0,5}: {'7': {}, '3': {}},
-		{0,6}: {'8': {}, '3': {}},
-		{0,7}: {'6': {}, '3': {}},
-		{0,8}: {'9': {}, '3': {}},
-		{0,9}: {'3': {}, '6': {}},
-	}
-
-	excludeByNotPossible(pos,notSolvePos, notSolvePosPossibleMap)
-
-	t.Logf("this pos possible is: [%v]", notSolvePosPossibleMap[pos])
-
 }
